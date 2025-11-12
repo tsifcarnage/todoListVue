2025ac62d0002edad33da34942b3cd89578e6aea
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-11-12 21:39:39 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import HelloWorld from "./components/todoList.vue";
 export default {
   data() {
     return {
+      nom: "",
       hiding: false,
     };
   },
@@ -17,17 +18,25 @@ export default {
 <template>
   <div>
     <h1 class="m-2 text-center text-5xl text-red-700">Veille todoList</h1>
-    <h4>
+    <h4 class="nameForm" v-if="hiding === false">
       Bonjour:
       <input
         @keyup.enter="submit"
         class="border-none outline-none"
-        id="nom"
+        v-model="nom"
         type="text"
         placeholder="Votre nom"
       />
     </h4>
+    <h4 v-else class="text-green-800 text-center text-3xl">
+      Bonjour {{ nom }}
+    </h4>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.nameForm {
+  text-align: center;
+  font-size: 1.2rem;
+}
+</style>
