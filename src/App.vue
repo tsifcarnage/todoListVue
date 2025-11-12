5685f1a5a155750da7eaf236e60830bc0d7e694d
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-11-12 21:22:51 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import HelloWorld from "./components/todoList.vue";
+export default {
+  data() {
+    return {
+      hiding: false,
+    };
+  },
+  methods: {
+    submit() {
+      this.hiding = true;
+    },
+  },
+};
 </script>
 
 <template>
-  <div
-    class="flex flex-col gap-2 justify-center border-2 border-primary p-2 w-lg m-auto mt-[10%]"
-  >
-    <HelloWorld class="text-center" />
-    <button class="btn text btn-neutral px-5 py-1">hey</button>
+  <div>
+    <h1 class="m-2 text-center text-5xl text-red-700">Veille todoList</h1>
+    <h4>
+      Bonjour:
+      <input
+        @keyup.enter="submit"
+        class="border-none outline-none"
+        id="nom"
+        type="text"
+        placeholder="Votre nom"
+      />
+    </h4>
   </div>
 </template>
 
