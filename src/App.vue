503a8ca0a9160bf757b2e9008768f476307a93f5
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-11-12 18:07:12 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,12 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="card bg-base-100 w-96 shadow-sm">
-    <HelloWorld></HelloWorld>
-    <button class="btn btn-accent px-5 py-1">hey</button>
+  <div
+    class="flex flex-col gap-2 justify-center border-2 border-primary p-2 w-lg m-auto mt-[10%]"
+  >
+    <HelloWorld class="text-center" />
+    <button class="btn text btn-neutral px-5 py-1">hey</button>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
