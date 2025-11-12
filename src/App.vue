7c4cb3d8a509dfe8bb84bd8af5bf0161158b0db6
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-11-12 15:55:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="">
+    <h1 class="text-lg text-amber-800">Hello world</h1>
+    <button class="btn btn-accent px-5 py-1">hey</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
