60451027dd950a0f7b10cdaaa8d6f8201389bb98
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-11-17 21:17:58 +0100

    Update Todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -13,18 +13,35 @@
         placeholder="Nouvelle tache"
         required
       />
-      <button class="btn btn-accent" type="submit">Ajouter</button>
-    </form>
-    <ul
-      class="bg-base-200 my-2 p-2 flex justify-between gap-2 text-center text-lg"
-      v-for="(task, index) in tasks"
-      :key="index"
-    >
-      <li class="self-center">$ {{ task }}</li>
-      <div>
-        <button class="btn btn-success mr-2">Tache fini</button>
-        <button @click="delUser(index)" class="btn btn-error">Supprimer</button>
+      <div class="flex flex-wrap gap-1">
+        <button class="btn btn-accent flex-1" type="submit">Ajouter</button>
+        <button @click="delAll()" class="btn btn-error flex-1" type="button">
+          Tout supprimer
+        </button>
       </div>
+    </form>
+    <ul v-for="(task, index) in tasks" :key="index">
+      <section
+        class="bg-base-200 my-2 p-2 flex justify-between gap-2 text-center text-lg"
+        :class="{ 'bg-green-600 text-black': task.ending }"
+      >
+        <li class="self-center">$ {{ task.text }}</li>
+        <div>
+          <button
+            v-if="task.ending"
+            @click="backTask(index)"
+            class="btn btn-warning mr-2"
+          >
+            Annuler
+          </button>
+          <button v-else @click="endTask(index)" class="btn btn-success mr-2">
+            Tache fini
+          </button>
+          <button @click="delUser(index)" class="btn btn-error">
+            Supprimer
+          </button>
+        </div>
+      </section>
     </ul>
   </div>
 </template>
@@ -35,16 +52,28 @@ export default {
     return {
       add: "",
       tasks: [],
+      ending: false,
     };
   },
   methods: {
     addUser() {
-      this.tasks.push(this.add);
+      this.tasks.push({
+        text: this.add,
+        ending: false,
+      });
       this.add = "";
     },
     delUser(index) {
-      this.tasks = this.tasks.filter((_, taskid) => taskid != index);
-      this.add = "";
+      this.tasks.splice(index, 1);
+    },
+    endTask(index) {
+      this.tasks[index].ending = true;
+    },
+    backTask(index) {
+      this.tasks[index].ending = false;
+    },
+    delAll() {
+      this.tasks.splice(0, this.tasks.length);
     },
   },
 };
