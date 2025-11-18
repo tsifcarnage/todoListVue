ee524558c564b1da51667a8f23930ad7992da570
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-11-18 11:59:04 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
 import Todo from "./components/Todo.vue";
+import Theme from "./components/Theme.vue";
 export default {
   components: {
     Todo,
+    Theme,
   },
   data() {
     return {
@@ -20,7 +22,11 @@ export default {
 
 <template>
   <div>
-    <h1 class="m-2 text-center text-5xl text-red-700">Veille todoList</h1>
+    <div class="flex justify-between px-3">
+      <div></div>
+      <h1 class="m-2 text-center text-5xl text-red-700">Veille todoList</h1>
+      <Theme class="self-center" />
+    </div>
     <h4 class="nameForm mt-5" v-if="hiding === false">
       Bonjour:
       <input
